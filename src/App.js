efa4f2012a7f6405429d3caf3b30747ce9fef86f
David Hou <hyuanwei@gmail.com>
2022-07-12 00:00:57 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selected, setSelected] = useState(null);
+
+    const toggle = (i) => {
+        if (selected === i) {
+            return setSelected(null);
+        }
+
+        setSelected(i);
+    };
+
+    return (
+        <div className="wrapper">
+            <div className="accordion">
+                {data.map((item, i) => (
+                    <div className="item" key={i}>
+                        <div className="title" onClick={() => toggle(i)}>
+                            <h2>{item.question}</h2>
+                            <span>{selected === i ? "-" : "+"}</span>
+                        </div>
+                        <div
+                            className={
+                                selected === i ? "content show" : "content"
+                            }
+                        >
+                            {item.answer}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
+
+const data = [
+    {
+        question: "What is Lorem Ipsum?",
+        answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+        question: "Why do we use it?",
+        answer: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    },
+    {
+        question: "Where can I get some?",
+        answer: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+    },
+];
 
 export default App;
